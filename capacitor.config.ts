bcf129a740f075f32f82ba28897176f7ac873001
reznov11 <reznov110@gmail.com>
2024-05-01 11:29:06 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'emotioniq',
+  webDir: 'www'
+};
+
+export default config;
