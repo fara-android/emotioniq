45b96aa51ade218e1dff0c315c5dea4da968e790
Ammar <reznov110@gmail.com>
2024-05-10 10:01:22 +0600

    Temporarly removing glassfy

--- src/app/services/premium.service.ts
+++ src/app/services/premium.service.ts
@@ -1,67 +1,67 @@
-import { environment } from './../../environments/environment';
-import { Injectable } from '@angular/core';
-import {
-	Glassfy,
-	GlassfyOffering,
-	GlassfyPermission,
-	GlassfySku,
-	GlassfyTransaction
-} from 'capacitor-plugin-glassfy';
-import { BehaviorSubject } from 'rxjs';
-import { AlertController, ToastController } from '@ionic/angular';
+// import { environment } from './../../environments/environment';
+// import { Injectable } from '@angular/core';
+// import {
+// 	Glassfy,
+// 	GlassfyOffering,
+// 	GlassfyPermission,
+// 	GlassfySku,
+// 	GlassfyTransaction
+// } from 'capacitor-plugin-glassfy';
+// import { BehaviorSubject } from 'rxjs';
+// import { AlertController, ToastController } from '@ionic/angular';
 
-@Injectable({
-	providedIn: 'root'
-})
-export class ProductService {
-	// Init an "empty" user
-	user = new BehaviorSubject({ gems: 0, skins: [], pro: false });
+// @Injectable({
+// 	providedIn: 'root'
+// })
+// export class ProductService {
+// 	// Init an "empty" user
+// 	user = new BehaviorSubject({ gems: 0, skins: [], pro: false });
 
-	private offerings: BehaviorSubject<GlassfyOffering[]> = new BehaviorSubject<GlassfyOffering[]>([]);
+// 	private offerings: BehaviorSubject<GlassfyOffering[]> = new BehaviorSubject<GlassfyOffering[]>([]);
 
-	constructor(private toastController: ToastController, private alertController: AlertController) {
-		this.initGlassfy();
-	}
+// 	constructor(private toastController: ToastController, private alertController: AlertController) {
+// 		this.initGlassfy();
+// 	}
 
-	async initGlassfy() {
-		try {
-			// Initialise Glassfy
-			await Glassfy.initialize({
-				apiKey: environment.glassfyKey,
-				watcherMode: false
-			});
+// 	async initGlassfy() {
+// 		try {
+// 			// Initialise Glassfy
+// 			await Glassfy.initialize({
+// 				apiKey: environment.glassfyKey,
+// 				watcherMode: false
+// 			});
 
-			// Get all user permissions
-			const permissions = await Glassfy.permissions();
-			this.handleExistingPermissions(permissions.all);
+// 			// Get all user permissions
+// 			const permissions = await Glassfy.permissions();
+// 			this.handleExistingPermissions(permissions.all);
 
-			// Get all offerings (products)
-			const offerings = await Glassfy.offerings();
-			this.offerings.next(offerings.all);
-		} catch (e) {
-			console.log('init error: ', e);
-		}
-	}
+// 			// Get all offerings (products)
+// 			const offerings = await Glassfy.offerings();
+// 			this.offerings.next(offerings.all);
+// 		} catch (e) {
+// 			console.log('init error: ', e);
+// 		}
+// 	}
 
-	async purchase(sku: GlassfySku) {
-		// TODO
-	}
+// 	async purchase(sku: GlassfySku) {
+// 		// TODO
+// 	}
 
-	handleExistingPermissions(permissions: GlassfyPermission[]) {
-		// TODO
-	}
+// 	handleExistingPermissions(permissions: GlassfyPermission[]) {
+// 		// TODO
+// 	}
 
-	async handleSuccessfulTransactionResult(transaction: GlassfyTransaction, sku: GlassfySku) {
-		// TODO
-	}
+// 	async handleSuccessfulTransactionResult(transaction: GlassfyTransaction, sku: GlassfySku) {
+// 		// TODO
+// 	}
 
-	// Helper functions
-	getOfferings() {
-		return this.offerings.asObservable();
-	}
+// 	// Helper functions
+// 	getOfferings() {
+// 		return this.offerings.asObservable();
+// 	}
 
-	async restore() {
-		const permissions = await Glassfy.restorePurchases();
-		// Handle those permissions!
-	}
-}
+// 	async restore() {
+// 		const permissions = await Glassfy.restorePurchases();
+// 		// Handle those permissions!
+// 	}
+// }
